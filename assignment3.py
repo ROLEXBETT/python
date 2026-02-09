bd3b818f946be518ade196967b32694e9bd4d490
ROLEXBETT <rolexbett6684@gmail.com>
2026-02-09 11:33:56 +0300

    correction

--- assignment3.py
+++ assignment3.py
@@ -1,29 +1,22 @@
-
-# Store gross salary in a variable
-gross_salary = float(input("Enter your gross salary (Ksh): "))
-
-# Determine monthly contribution using if-else statements
-if gross_salary < 6000:
-    contribution = 150
-elif gross_salary <= 7999:
-    contribution = 300
-elif gross_salary <= 11999:
-    contribution = 400
-elif gross_salary <= 14999:
-    contribution = 500
-elif gross_salary <= 19999:
-    contribution = 600
-elif gross_salary <= 24999:
-    contribution = 750
-elif gross_salary <= 29999:
-    contribution = 850
-elif gross_salary <= 49999:
-    contribution = 1000
-elif gross_salary <= 99999:
-    contribution = 1500
+# Below is a program that calculates the monthly contribution an employee should make to the National Health Insuarance as per the grossincome
+grosssalary = int(input("Enter your salary here: "))
+if grosssalary > 0 and grosssalary <= 5999:
+    print("Your monthly contribution is Ksh 150.00")
+elif grosssalary > 5999 and grosssalary <= 7999:
+    print("Your monthly contribution is Ksh 300.00")
+elif grosssalary > 7999 and grosssalary <= 11999:
+    print("Your monthly contribution is Ksh 400.00")
+elif grosssalary > 11999 and grosssalary <= 14999:
+    print("Your monthly contribution is Ksh 500.00")
+elif grosssalary > 14999 and grosssalary <= 19999:
+    print("Your monthly contribution is Ksh 600.00")
+elif grosssalary > 19999 and grosssalary <= 24999:
+    print("Your monthly contribution is Ksh 750.00")
+elif grosssalary > 24999 and grosssalary <= 29999:
+    print("Your monthly contribution is Ksh 850.00")
+elif grosssalary > 29999 and grosssalary <= 49999:
+    print("Your monthly contribution is Ksh 1000.00")
+elif grosssalary > 49999 and grosssalary <= 99999:
+    print("Your monthly contribution is Ksh 1500.00")
 else:
-    contribution = 2000
-
-# Output the result
-print(f"Your NHIF monthly contribution is: Ksh {contribution}")
-
+    print("Your monthly contribution is Ksh 2000.00")
