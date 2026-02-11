a9aba94e2ca5b9973c7afaa7774c3be1b2163c62
ROLEXBETT <rolexbett6684@gmail.com>
2026-02-12 02:33:08 +0300

    lesson 6

---
+++ lesson6a.py
@@ -0,0 +1,23 @@
+# Python module
+# A python module is afile that contains python definitions, statements, and or functions.
+
+def add():
+    num1 = 20
+    num2 = 30
+    sum = num1 + num2
+    print("The answer is", sum)
+
+def subtract():
+        x = 45
+        y = 30
+        difference = x-y
+        print("The diffence is: ", difference)
+
+def rectangleArea():
+        length = int(input("Enter the length of the rectangle: "))
+        width = int(input("Enter the width of the rectangle: "))
+
+        area = length * width
+        print("the area of the rectangle is: ", area)
+
+    # This functions defined above on this particular file they can be called into another file
---
+++ lesson6d.py
@@ -0,0 +1,12 @@
+# Modules mumber 2: In built modules
+# the come pre-installed with the python interpreter
+from math import sqrt, factorial
+
+# import math
+import datetime
+
+#math.factorial
+
+print(sqrt(81))
+print(factorial(10))
+print(datetime.datetime.now())
